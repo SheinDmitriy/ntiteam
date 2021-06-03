c53e3899274fc1edabaf60b99d62f3af80474477
Dmitriy <dmitriy.shein86@gmail.com>
2021-06-03 21:48:57 +0500

    Создал сущность Planet(Планета)

---
+++ src/main/java/shein/dmitriy/ntiteam/entitys/Planet.java
@@ -0,0 +1,23 @@
+package shein.dmitriy.ntiteam.entitys;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tplanet")
+@Data
+public class Planet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "planet_id")
+    private int planetId;
+
+    @Column(name = "name")
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "governor_id")
+    private int governorId;
+}
