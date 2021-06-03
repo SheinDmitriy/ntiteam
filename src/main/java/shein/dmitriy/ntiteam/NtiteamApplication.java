0566d97d49cf8bce36a6233d212e368172921fcf
Dmitriy <dmitriy.shein86@gmail.com>
2021-06-03 20:30:06 +0500

    init

---
+++ src/main/java/shein/dmitriy/ntiteam/NtiteamApplication.java
@@ -0,0 +1,13 @@
+package shein.dmitriy.ntiteam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NtiteamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NtiteamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/shein/dmitriy/ntiteam/NtiteamApplicationTests.java
@@ -0,0 +1,13 @@
+package shein.dmitriy.ntiteam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NtiteamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
